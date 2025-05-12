950002bf6630e860809f6d071e579523a0882600
aralbai <xabibullayevmm@gmail.com>
2025-05-12 16:35:49 +0500

    mongodb jalgandi

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import mongoose from "mongoose";
 
 const app = express();
 
+mongoose
+  .connect("mongodb://0.0.0.0/test")
+  .then(() => {
+    console.log("MongoDB connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(5000, () => {
   console.log("Server is running 5000...");
 });
